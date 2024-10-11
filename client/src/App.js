7f486b6361fce87ecbc2735c6d531547ab377957
Subodh20482 <149533665+Subodh20482@users.noreply.github.com>
2024-10-11 18:12:17 +0530

    front

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbaar from './components/header/Navbaar';
+import Newnav from './components/newnavbaar/Newnav';
+import Maincomp from './components/home/Maincomp';
+function App() {
+  return (
+    <>
+   <Navbaar />
+   <Newnav />
+   <Maincomp />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/newnavbaar/Newnav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "..//newnavbaar/newnav.css";
+const Newnav = () => {
+  return (
+    <div className='new_nav'>
+        <div className="nav_data">
+            <div className="left_data">
+                <p>All</p>
+                <p>Mobile</p>
+                <p>Bestseller</p>
+                <p>Fasion</p>
+                <p>Customer Services</p>
+                <p>Electronics</p>
+                <p>Prime</p>
+                <p>Today's deal</p>
+                <p>Amazon Pay</p>
+            </div>
+            <div className="left_data">
+                <img src="./nav.jpg" alt="navdata" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Newnav
